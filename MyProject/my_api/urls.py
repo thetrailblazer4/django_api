560ebcc7bfa553e15d58e35661e1aaa6ceffa32e
mukesh <mukeshamra@hotmail.com>
2022-02-03 22:34:56 -0500

    added Model serializers

--- MyProject/my_api/urls.py
+++ MyProject/my_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . views import article_list
+from . views import article_list, article_detail
 
 urlpatterns = [
     path('article/', article_list, name='article'),
+    path('article/<int:pk>/', article_detail, name='article-detail'),
 ]
